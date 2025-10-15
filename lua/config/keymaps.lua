add2890a8f9433f583e339da56a388593d454515
Caleb <cstclair@brandsinsurance.com>
2025-10-15 16:55:00 -0400

    2025-10-15_16-55-00

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,13 @@
 
 vim.keymap.set("v", ">", ">gv", { desc = "Indent and stay in visual mode" })
 vim.keymap.set("v", "<", "<gv", { desc = "Outdent and stay in visual mode" })
+
+vim.keymap.set("n", "<leader>yc", function()
+  local diag = vim.diagnostic.get(0, { lnum = vim.fn.line(".") - 1 })[1]
+  if diag then
+    vim.fn.setreg("+", diag.message)
+    vim.notify("Copied diagnostic: " .. diag.message)
+  else
+    vim.notify("No diagnostic on this line")
+  end
+end, { desc = "Copy diagnostic under cursor" })
